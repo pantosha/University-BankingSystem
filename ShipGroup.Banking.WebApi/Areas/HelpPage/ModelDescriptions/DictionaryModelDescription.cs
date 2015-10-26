0b9e674c61c75f1b403f3854bb63d48581bb5225
AndreyIgnatenko <ravingdev@gmail.com>
2015-10-26 17:06:21 +0300

    Initial Solution Structure: Common, WebApi, EmployeeClient

---
+++ ShipGroup.Banking.WebApi/Areas/HelpPage/ModelDescriptions/DictionaryModelDescription.cs
@@ -0,0 +1,6 @@
+namespace ShipGroup.Banking.WebApi.Areas.HelpPage.ModelDescriptions
+{
+    public class DictionaryModelDescription : KeyValuePairModelDescription
+    {
+    }
+}
---
+++ ShipGroup.Banking.WebApi/Areas/HelpPage/ModelDescriptions/KeyValuePairModelDescription.cs
@@ -0,0 +1,9 @@
+namespace ShipGroup.Banking.WebApi.Areas.HelpPage.ModelDescriptions
+{
+    public class KeyValuePairModelDescription : ModelDescription
+    {
+        public ModelDescription KeyModelDescription { get; set; }
+
+        public ModelDescription ValueModelDescription { get; set; }
+    }
+}
